7d5c819d4df3e7f9f74ab1f35bde5a0a3ab48ceb
Alana <alanasifasam@gmail.com>
2021-06-04 17:22:49 -0300

    teste conexão com banco

--- server.js
+++ server.js
@@ -6,41 +6,40 @@ const mongoose = require('mongoose');
 const routes = require('./src/routes');
 
 
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+const atlas_connection_string = "mongodb+srv://root:1234@alana01.w65ds.mongodb.net/curso-basico-mern?retryWrites=true&w=majority";
+mongoose.connect(atlas_connection_string,{
+  useUnifiedTopology:true,
+  useNewUrlParser : true,
+  useFindAndModify :false
 
-const MongoClient = require('mongodb').MongoClient;
- const uri = "mongodb+srv://root:1234@alana01.w65ds.mongodb.net/curso-basico-mern?retryWrites=true&w=majority";
-  
- const client = new MongoClient(uri, {
- useNewUrlParser: true, 
- useUnifiedTopology: true });
-  
-  client.connect(err => {
-  const collection = client.db("test").collection("devices");
+}), function(err){
+  if(err){
+    console.log(err)
+  }else{
+     console.log('MongoDB CONECTADO COM SUCESSO !!')
+  }
 
-    client.close();
-  
-  });
+  }
 
-app.use(cors());
-app.use(cookieParser());
-app.use(express.json());
-app.use(routes);
+
+
+  app.use(cors());
+  app.use(cookieParser());
+  app.use(express.json());
+  
+  
+
 
 app.get('/',function(req,res){
   res.json({message:'hello world'});
 });
-
 app.use(routes);
 
 app.listen(port,function(){
   console.log(`Server runing on port ${port}`)
-})
-
-
-
-
-
-
+});
